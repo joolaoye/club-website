53853ea79582bf4fd7c0078809f226f4bc341f4c
Joshua Olaoye <afrokid010404@gmail.com>
2025-09-21 20:37:32 -0500

    chore: fix api url in api client

--- packages/api-client/src/transport/http-transport.ts
+++ packages/api-client/src/transport/http-transport.ts
@@ -21,7 +21,11 @@ export class HttpTransport {
     private getToken?: () => Promise<string | null>;
 
     constructor(options: TransportOptions = {}) {
-        this.baseUrl = (options.baseUrl || 'http://localhost:8000/api').replace(/\/$/, '');
+        const defaultBaseUrl = typeof window !== 'undefined' 
+            ? process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api'
+            : 'http://localhost:8000/api';
+
+        this.baseUrl = (options.baseUrl || defaultBaseUrl).replace(/\/$/, '');
         this.defaultTimeout = options.timeout || 10000;
         this.defaultRetries = options.retries || 3;
         this.getToken = options.getToken;
